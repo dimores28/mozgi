9885cd5df31e7b47c91ab20a32136ac4ef0cd664
Dmytro <dimores95@gmail.com>
2022-08-19 14:20:31 +0300

    burger menu was added

---
+++ src/components/ui/v-baner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="v-baner baner">
+    <h1 class="baner__text">FULL-CYCLE EVENT AGENCY</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-baner"
+};
+</script>
+
+<style lang="less">
+.baner {
+  //   margin: 0 auto;
+
+  &__text {
+    font-size: 82px;
+    line-height: 90px;
+    text-align: center;
+    max-width: 1023px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .baner {
+    &__text {
+      font-size: 52px;
+      line-height: 64px;
+    }
+  }
+}
+
+@media screen and (max-width: 440px) {
+  .baner {
+    &__text {
+      font-size: 24px;
+      line-height: 38px;
+    }
+  }
+}
+</style>
