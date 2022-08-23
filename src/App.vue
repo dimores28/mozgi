33eece7ebea06c3332b0909c14e19af29eaa4e6e
Dmytro <dmytroivanovichn@gmail.com>
2022-08-23 11:43:21 +0300

    made an example of animation of the movement of the background image

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <img
+    src="@/assets/images/background_text.png"
+    alt="bg img"
+    class="animate-bg-img"
+  />
   <div class="container posr">
     <router-view v-slot="{ Component, route }">
       <transition :name="route.meta.transition || 'fade'">
@@ -15,4 +20,33 @@ export default {
 };
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+#app {
+  overflow: hidden;
+}
+
+.animate-bg-img {
+  border: 1px solid red;
+  position: fixed;
+  top: -862px;
+  left: 1440px;
+  animation: examples 15s infinite;
+}
+
+@keyframes example {
+  0% {
+    margin: 0vh;
+  }
+  100% {
+    margin: 100vh;
+  }
+}
+
+@keyframes examples {
+  100% {
+    left: -1440px;
+    top: 100vh;
+  }
+}
+
+</style>
