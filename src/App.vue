56ddc909b150eb0d4d05c5dc78aa27e5613ecf75
Dmytro <dimores95@gmail.com>
2022-08-26 14:58:02 +0300

    background text animation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <img
+    src="@/assets/images/topdown_img.png"
+    alt="bg image"
+    class="bg-animate-img bg-animate-img__dn"
+  />
+  <img
+    src="@/assets/images/upwards_img.png"
+    alt="bg image"
+    class="bg-animate-img bg-animate-img__up"
+  />
   <div class="container posr">
     <router-view v-slot="{ Component, route }">
       <transition :name="route.meta.transition || 'fade'">
